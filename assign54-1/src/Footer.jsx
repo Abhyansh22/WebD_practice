8312cf8f6427d8c12db3ade5b99225be26181ab9
Abhyansh22 <abhyansh22@gmail.com>
2025-08-21 19:19:31 +0530

    change in footer

--- assign54-1/src/Footer.jsx
+++ assign54-1/src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-screen h-20 bg-gray-600 flex justify-between items-center'>
+    <div className=' h-20 bg-gray-600 flex justify-between items-center'>
       <p className='text-xs text-white ml-24'>Copyright 2022 | CodeYogi</p>
       <p className='text-xs text-white mr-24'>Powered By CodeYogi</p>
     </div>
