2465859a78c6033ff0e66b5dc281d837a13f530f
Abhyansh22 <abhyansh22@gmail.com>
2025-08-23 20:44:12 +0530

    Did product card changes

--- assign54-1/src/App.jsx
+++ assign54-1/src/App.jsx
@@ -12,15 +12,15 @@ function App() {
           <option vlaue="">Default Sorting</option>
         </select>
         <div className='flex flex-wrap justify-between'>
-          < Product price="15.00" desc="Transparent Coffee Mug" category="Mugs" image="https://codeyogi.io/coffee-mug.jpeg" />
-          < Product price="19.00" desc="Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-blue.jpg"/>
-          < Product price="19.00" desc="Family Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-coffee-600x600.jpg"/>
-          < Product price="19.00" desc="White Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt5-600x600.jpg"/>
-          < Product price="19.00" desc="Tea Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-yellow-600x600.jpg"/>
-          < Product price="19.00" desc="Traditional Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt2-600x600.jpg"/>
-          < Product price="19.00" desc="Girls Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-red-600x600.jpg"/>
-          < Product price="19.00" desc="Pink Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt7-600x600.jpg"/>
-          < Product price="19.00" desc="Red Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt4-600x600.jpg"/>
+          < Product price="15.00" desc="White Coffee Mug" category="Mugs" image="https://codeyogi.io/coffee-mug.jpeg" />
+          < Product price="19.00" desc="Blue Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-blue.jpg"/>
+          < Product price="15.00" desc="Brown Coffee Mug"  category="Mug "  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-coffee-600x600.jpg"/>
+          < Product price="34.00" desc="Green Tshirt"  category="Tshirt"  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt5-600x600.jpg"/>
+          < Product price="18.00" desc="Yellor Coffee Mug"  category="Mug"  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-yellow-600x600.jpg"/>
+          < Product price="34.00" desc="Light-Blue Tshirt"  category="Tshirt"  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt2-600x600.jpg"/>
+          < Product price="14.00" desc="Red Coffee Mug"  category="Mug"  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-red-600x600.jpg"/>
+          < Product price="25.00" desc="Orange Tshirt"  category="Tshirt"  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt7-600x600.jpg"/>
+          < Product price="25.00" desc="Blue Tshirt"  category="Tshirt"  image="https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt4-600x600.jpg"/>
         </div>
         <div className='flex gap-2'>
           <a className="pt-2 px-3 border-red-500 border-2 hover:bg-red-500 hover:text-white" href="#">1</a>
