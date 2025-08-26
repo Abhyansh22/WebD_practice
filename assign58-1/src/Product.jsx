d160c5ee57674fcca04f475d2be5f88a234339dd
Abhyansh22 <abhyansh22@gmail.com>
2025-08-26 23:44:00 +0530

    Did assignment 58.1

---
+++ assign58-1/src/Product.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Product(props){
+    return(
+        <div className="w-72 m-5 gap-1 flex flex-col pb-5 grow">
+            <img className="w-100% max-w-2xs h-72 object-cover" src={props.image} alt=""/>
+            <p className="text-gray-400 text-xs">{props.category}</p>
+            <h3 className="font-bold text-gray-600">{props.desc}</h3>
+            <p className="text-gray-600">${props.price}</p>
+            <Link to="/ProductCard" className="bg-blue-500 rounded-xl py-3 px-6 mx-3 grow text-center hover:ring-2 hover:ring-blue-500 hover:ring-offset-2 hover:text-white">View details</Link>
+        </div>
+    )
+
+}
+
+export default Product
---
+++ assign58-1/src/ProductList.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+import Product from "./Product";
+
+function ProductList(){
+    const [query, setQuery] = useState("")
+  const [selected, setSelected] = useState("default")
+
+  const inventory=[{price:"15.00", desc:"White Coffee Mug", category:"Mugs", image:"https://codeyogi.io/coffee-mug.jpeg"} ,
+          {price:"19.00", desc:"Blue Coffee Mug", category:"Mug", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-blue.jpg"},
+          {price:"15.00", desc:"Brown Coffee Mug",  category:"Mug", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-coffee-600x600.jpg"},  
+          {price:"34.00", desc:"Green Tshirt",  category:"Tshirt", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt5-600x600.jpg"},
+          {price:"18.00", desc:"Yellor Coffee Mug",  category:"Mug", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-yellow-600x600.jpg"},
+          {price:"34.00", desc:"Light-Blue Tshirt",  category:"Tshirt", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt2-600x600.jpg"},  
+          {price:"14.00", desc:"Red Coffee Mug",  category:"Mug", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/mug-red-600x600.jpg"}, 
+          {price:"25.00", desc:"Orange Tshirt",  category:"Tshirt", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt7-600x600.jpg"}, 
+          {price:"25.00", desc:"Blue Tshirt", category:"Tshirt", image:"https://trycasuals.ezyro.com/wp-content/uploads/2018/06/tshirt4-600x600.jpg"}];
+
+
+  function optionChange(event){
+    setSelected(event.target.value)
+  }
+  function textChange(event){
+    setQuery((event.target.value).toLowerCase())
+  }
+
+  const filteredInventory =inventory.filter(function (item){
+    if(item.desc.toLowerCase().indexOf(query)!=-1){
+      return true;
+    }else{
+      return false;
+    }
+  })
+
+  if (selected=="title"){
+    filteredInventory.sort(function(x,y){
+      return((x.category<y.category)?-1:1)
+    })
+  }else if(selected=="ltoh"){
+    filteredInventory.sort(function(x,y){
+      return((+x.price-(+y.price))<0 ?-1:1)
+    })
+  }else if(selected=="htol"){
+    filteredInventory.sort(function(x,y){
+      return((+x.price-(+y.price))>0 ?-1:1)
+    })
+  }
+
+    return(
+        <div className="flex flex-col">
+            <div className='flex gap-4 self-end '>
+                <input type='text'onChange={textChange} className='border-gray-200 border-2 rounded-md px-6' placeholder='Search'/>
+                <select onChange={optionChange} className='border-gray-200 border-2 py-2 pl-3 pr-8 bg-gray-100 text-gray-600' name="sortTag">
+                    <option value="default">Default Sorting</option>
+                    <option value="title">Sort by title</option>
+                    <option value="ltoh">Sort by price: low to high</option>
+                    <option value="htol">Sort by price: high to low</option>
+                </select>
+            </div>
+            <div className="flex flex-wrap justify-between grow">
+                {filteredInventory.map(function(item){
+                    return(<Product price={item.price} desc={item.desc} category={item.category} image={item.image}></Product>)
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default ProductList
